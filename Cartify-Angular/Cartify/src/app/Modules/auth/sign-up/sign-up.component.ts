eb7f3be957c185645885a98a1d66ab2fff0c5ac6
Riya-Gupta-1999 <riyagupta23041999@gmail.com>
2025-03-04 17:29:07 +0530

    validations on sign up page

--- Cartify-Angular/Cartify/src/app/Modules/auth/sign-up/sign-up.component.ts
+++ Cartify-Angular/Cartify/src/app/Modules/auth/sign-up/sign-up.component.ts
@@ -9,7 +9,10 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
   public emailValidations: string;
-
+  public passwordValidations: string;
+  public nameValidations: string;
+  public phoneValidation: string;
+  public addressValidation: string;
   constructor(private fb: FormBuilder) {
    }
 
@@ -19,14 +22,30 @@ export class SignUpComponent implements OnInit {
     email.valueChanges.subscribe(x => {
      this.validateEmail(email);
     });
+    const password = this.signUpForm.get('password');
+    password.valueChanges.subscribe(x => {
+     this.validatePassword(password);
+    });
+    const name = this.signUpForm.get('name');
+    name.valueChanges.subscribe(x => {
+     this.validateName(name);
+    });
+    const phone = this.signUpForm.get('phoneNumber');
+    phone.valueChanges.subscribe(x => {
+     this.validatePhone(phone);
+    });
+    const address = this.signUpForm.get('address');
+    address.valueChanges.subscribe(x => {
+     this.validateAddress(address);
+    });
   }
 
   private initForm(): void{
     this.signUpForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      name: ['', [Validators.required, Validators.minLength(20)]],
-      phoneNumber: ['', [Validators.required, Validators.minLength(10)]],
+      password: ['', [Validators.required]],
+      name: ['', [Validators.required, Validators.maxLength(20)]],
+      phoneNumber: ['', [Validators.required, Validators.maxLength(10)]],
       address: ['', [Validators.required]]
     });
   }
@@ -42,7 +61,38 @@ export class SignUpComponent implements OnInit {
       }
     }
   }
-
+  private validatePassword(password: AbstractControl): void{
+    this.passwordValidations = '';
+    if(password.errors && (password.touched || password.dirty)){
+      if(password.errors.required){
+        this.passwordValidations = 'This is required';
+      }
+    }
+  }
+  private validateName(name: AbstractControl): void{
+    this.nameValidations = '';
+    if(name.errors && (name.touched || name.dirty)){
+      if(name.errors.required){
+        this.nameValidations = 'This is required';
+      }
+    }
+  }
+  private validatePhone(phone: AbstractControl): void{
+    this.phoneValidation = '';
+    if(phone.errors && (phone.touched || phone.dirty)){
+      if(phone.errors.required){
+        this.phoneValidation = 'This is required';
+      }
+    }
+  }
+  private validateAddress(address: AbstractControl): void{
+    this.addressValidation = '';
+    if(address.errors && (address.touched || address.dirty)){
+      if(address.errors.required){
+        this.addressValidation = 'This is required';
+      }
+    }
+  }
   onSubmit() {
     // if (this.signUpForm.valid) {
     //   console.log('Login Data:', this.signUpForm.value);
