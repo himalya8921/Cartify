5ec4885c3ee66c0b8b92cda8613f17a221fcdbe3
Riya-Gupta-1999 <riyagupta23041999@gmail.com>
2025-03-20 12:17:47 +0530

    user modules routing

--- Cartify-Angular/Cartify/src/app/Modules/auth/Components/sign-in/sign-in.component.ts
+++ Cartify-Angular/Cartify/src/app/Modules/auth/Components/sign-in/sign-in.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { AuthModel } from 'src/app/Models/auth.model';
+import { UserDashboardRoutingModule } from 'src/app/Modules/user-dashboard/user-dashboard-routing.module';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -12,8 +14,10 @@ export class SignInComponent implements OnInit {
   loginForm: FormGroup;
   public emailValidations: string;
   public passwordValidations: string;
+  public roleId: string;
   constructor(private fb: FormBuilder,
-      private _authService : AuthService) {
+      private _authService : AuthService,
+      private router: Router) {
    }
 
   ngOnInit() {
@@ -63,9 +67,16 @@ export class SignInComponent implements OnInit {
         if (this.loginForm.valid) {
           this._authService.SignInUser(authModel).subscribe(response =>{localStorage.setItem('jwt_token', response.token);
             localStorage.setItem('role', response.role);
+            this.roleId = response.role;
             console.log(response.token);
             console.log(response.role);
+            console.log(this.roleId);
             alert('success');
+            if(this.roleId == "1"){
+              this.router.navigate(['/admin']);
+            }else if(this.roleId == "0"){
+              this.router.navigate(['/public']);
+            }
           }, 
           error => {
             alert('error');
--- Cartify-Angular/Cartify/src/app/Modules/user-dashboard/user-dashboard-routing.module.ts
+++ Cartify-Angular/Cartify/src/app/Modules/user-dashboard/user-dashboard-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './Components/admin/admin.component';
+import { PublicComponent } from './Components/public/public.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'admin',component: AdminComponent},
+  {path:'public',component: PublicComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
