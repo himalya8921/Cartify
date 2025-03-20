62e93c13032a8c3bf2e73f172bf82d9fc19af441
Riya-Gupta-1999 <riyagupta23041999@gmail.com>
2025-03-20 15:58:48 +0530

    product type data on angular

--- Cartify-Angular/Cartify/src/app/Modules/user-dashboard/Components/admin/admin.component.ts
+++ Cartify-Angular/Cartify/src/app/Modules/user-dashboard/Components/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDashboardService } from '../../services/user-dashboard.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
+  productTypes: any[] = [];
+  constructor(private _userDashboard : UserDashboardService) { }
 
   ngOnInit() {
+    this.getProductTypes();
   }
-
+  getProductTypes(): void {
+    this._userDashboard.GetAllProductType().subscribe({
+      next: (response) => {
+        if (response && response.message) {
+          this.productTypes = response.message; 
+        } else {
+          this.productTypes = []; 
+        }
+        console.log("Product Types:", this.productTypes);
+      },
+      error: (error) => {
+        console.error("Error fetching product types:", error);
+      }
+    });
+  }
 }
--- Cartify-Angular/Cartify/src/app/Modules/user-dashboard/services/user-dashboard.service.ts
+++ Cartify-Angular/Cartify/src/app/Modules/user-dashboard/services/user-dashboard.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
+import { productType } from 'src/app/Models/productType.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserDashboardService {
+private getProductTypeUrl = `${environment.apiUrl}/ProductType/GetAllProductTypes`;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  GetAllProductType(): Observable<any> {
+    return this.http.get<any>(this.getProductTypeUrl);
+  }
 }
